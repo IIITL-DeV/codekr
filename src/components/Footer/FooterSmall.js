f20dfff78aaa87dccb51e473544b36d774217a7c
prayutsu <gargabhay2000@gmail.com>
2021-10-07 23:38:07 +0530

    prettify

--- src/components/Footer/FooterSmall.js
+++ src/components/Footer/FooterSmall.js
@@ -21,31 +21,6 @@ function FooterSmall() {
     return (
         <div id="myfooter" className={`footerSmall m-0 ${(showFooter || window.pageYOffset === 0) ? "" : "hide"}`}>
             <div className="container-fluid p-4 m-0">
-                <div className="row justify-content-center">
-
-                    <div className="col-6 col-sm-6">
-                        <p style={{
-                            fontSize: '18px',
-                            padding: '0px',
-                            margin: '0px'
-                        }}></p>
-                    </div>
-
-
-                    <div className="col-6 col-sm-6 m-0">
-
-
-                        <div className="float-right m-0">
-
-                            <a className="social_media" target="_blank" href="https://www.facebook.com/practicewithcodedigger"><FontAwesomeIcon icon={faFacebook} size="2x" /></a>
-                            <a className="social_media" target="_blank" href="https://www.linkedin.com/company/codedigger"><FontAwesomeIcon icon={faLinkedin} size="2x" /></a>
-                            <a className="social_media" target="_blank" href="https://github.com/Code-dig-ger"><FontAwesomeIcon icon={faGithub} size="2x" /></a>
-                            <a className="social_media" target="_blank" href="https://www.youtube.com/channel/UCY5XRYpEGKT9cpzZmfWvh6A"><FontAwesomeIcon icon={faYoutube} size="2x" /></a>
-
-                        </div>
-                    </div>
-
-                </div>
 
             </div>
         </div>
--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -12,52 +12,8 @@ function Footer() {
     <div className="footer">
         <div className="container">
             <div className="row justify-content-center"> 
-                            
-                <div className="col-6 col-sm-3">
-                    <h5>Links</h5>
-                    <br/>
-                    <ul className="list-unstyled">
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/aboutus">About</Link></li>
-                       
-                        <li><Link to="/privacy">Privacy Policy</Link></li>
-                        <li><Link to="/terms">Terms of Service</Link></li>
-                    </ul>
-                </div>
-                <div className="col-12 col-sm-3 contactUs">
-                    <h5>Contact Us</h5>
-                    <br/>
-                    <div>
-                        
-		                <FontAwesomeIcon icon={faEnvelope} size="md"/> : <a href="mailto:contact.codedigger@gmail.com">
-                        contact.codedigger@gmail.com</a>
-                    </div>
-                    <a href="https://forms.gle/cL9ECdGDf6njYhV66" target="_blank">Feedback</a>
-                    
-                    
-                </div>
-
-                <div className="col-12 col-sm-3">
-                    
-                    
-                    <div>
-                        
-                    <h5>Follow Us</h5>
-                    <br/>
-                        <a target="_blank" className="social_media" href="https://www.facebook.com/practicewithcodedigger"><FontAwesomeIcon icon={faFacebook} size="2x"/></a>
-                        <a target="_blank" className="social_media" href="https://www.linkedin.com/company/codedigger"><FontAwesomeIcon icon={faLinkedin} size="2x"/></a>
-                        <a target="_blank" className="social_media" href="https://github.com/Code-dig-ger"><FontAwesomeIcon icon={faGithub} size="2x"/></a>
-                        <a target="_blank" className="social_media" href="https://www.youtube.com/channel/UCY5XRYpEGKT9cpzZmfWvh6A"><FontAwesomeIcon icon={faYoutube} size="2x"/></a>
-                        
-                    </div>
-                </div>
-                
             </div>
-            <div className="row justify-content-center">             
-                <div className="col-auto">
-                    <p>© Copyright Codedigger 2021</p>
-                </div>
-            </div>
+            
         </div>
     </div>
     )
